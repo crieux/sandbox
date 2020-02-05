32a8aa234a7de49a2c9a4bb66e38b823e2309dd3
Charlotte Rieux <charlotte.rieux44@yahoo.fr>
2020-02-05 10:56:54 +0100

    Create config folder

--- pet_projects/dashboards/config.py
+++ pet_projects/dashboards/config.py
@@ -10,13 +10,14 @@ import os
 ################################################################################
 
 INPUT_DATA = Path("inputs/")
+CONFIG_DATA = Path("config/")
 OUTPUT_DATA = Path("outputs/")
 
 # iris.py
 IRIS_DATA_PATH = os.path.join(INPUT_DATA, "iris.csv")
 
 # open_data_nantes.py
-MAP_TOKEN = os.path.join(INPUT_DATA, "map_token")
+MAP_TOKEN = os.path.join(CONFIG_DATA, "map_token")
 NANTES_DISTRICTS_INFO = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_quartiers-nantes&rows=-1&facet=nom"
 NANTES_PARKINGS_INFO = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes&rows=-1&facet=libcategorie&facet=libtype&facet=acces_pmr&facet=service_velo&facet=stationnement_velo&facet=stationnement_velo_securise&facet=moyen_paiement"
 NANTES_PARKINGS_AVAILABILITY = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_parkings-publics-nantes-disponibilites&rows=-1&facet=grp_nom&facet=grp_statut"
